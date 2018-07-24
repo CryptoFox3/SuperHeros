f28459bf2e4139ab7d64f8dc51d9a42c349ca45d
cryptofox3 <pghodgson558@gmail.com>
2018-07-24 15:53:44 -0500

    dding project files

---
+++ Superheros/Controllers/SuperHeroController.cs
@@ -0,0 +1,31 @@
+﻿using Superheros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Superheros.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListHeros()
+        {
+            List<Hero> superheros = new List<Hero>();
+
+            superheros.Add(new Hero { ID = 1, Name = "Spoderman", PrimaryAbility = " ", SecondaryAbility = " ", AlterEgo = " ", CatchPhrase = " ", });
+            superheros.Add(new Hero { ID = 1, Name = "Spoderman", PrimaryAbility = " ", SecondaryAbility = " ", AlterEgo = " ", CatchPhrase = " ", });
+            superheros.Add(new Hero { ID = 1, Name = "Spoderman", PrimaryAbility = " ", SecondaryAbility = " ", AlterEgo = " ", CatchPhrase = " ", });
+            superheros.Add(new Hero { ID = 1, Name = "Spoderman", PrimaryAbility = " ", SecondaryAbility = " ", AlterEgo = " ", CatchPhrase = " ", });
+            superheros.Add(new Hero { ID = 1, Name = "Spoderman", PrimaryAbility = " ", SecondaryAbility = " ", AlterEgo = " ", CatchPhrase = " ", });
+
+            return View();
+        }
+    }
+}
---
+++ Superheros/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Superheros.Models
+{
+    public class Person
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+
+    }
+}
